b964a6125f7f65a349b0b758518676f7bcc0fe5b
kayler empresa <kaylerdavid@gmail.com>
2023-04-17 17:39:46 +0200

    mejoras

--- springBasico/src/main/java/com/neoris/primer_proyecto/controlador/HomeControlador.java
+++ springBasico/src/main/java/com/neoris/primer_proyecto/controlador/HomeControlador.java
@@ -1,24 +1,36 @@
 package com.neoris.primer_proyecto.controlador;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.neoris.primer_proyecto.modelo.Direccion;
 import com.neoris.primer_proyecto.modelo.Persona;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/persona")
+//@RequestMapping("/datos") ESTO HACE QUE PARA ENTRAR USES LOCALHOST:8080/datos/persona||otros
 public class HomeControlador {
-	// localhost:8080/personas/datos
-	@RequestMapping(path = "/datos", method = RequestMethod.GET)
-	public Persona mostrarPersona(HttpServletRequest request) {
+	// localhost:8080/persona
+	@RequestMapping(path = "/persona", method = RequestMethod.GET)
+	public ResponseEntity mostrarPersona(HttpServletRequest request) {
 		Persona per = new Persona();
 		per.setNombre("juan");
 		per.setEdad(23);
-		per.setAficion("furbo");
-		return per;
+		per.setAficion("fuarbola");
+		return new ResponseEntity(per.getNombre() + " " + per.getEdad() + " " + per.getAficion(), HttpStatus.OK);
+		// return per; en el caso que el metodo sea public Persona
+	}
+
+	// localhost:8080/otros
+	@RequestMapping(path = "/otros", method = RequestMethod.GET)
+	public Direccion mostrarDireccion(HttpServletRequest request) {
+		Direccion dir = new Direccion();
+		dir.setDireccion("127.0.0.1");
+		return dir;
 	}
 
 }
---
+++ springBasico/src/main/java/com/neoris/primer_proyecto/modelo/Direccion.java
@@ -0,0 +1,32 @@
+package com.neoris.primer_proyecto.modelo;
+
+public class Direccion {
+	private String direccion;
+
+	/**
+	 * @param direccion
+	 */
+	public Direccion(String direccion) {
+		super();
+		this.direccion = direccion;
+	}
+
+	public Direccion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+}
